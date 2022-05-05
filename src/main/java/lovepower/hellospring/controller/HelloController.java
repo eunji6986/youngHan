1a469ea296d0c57a3f2e72d27bbc3c824d51d7a7
LEE EUNJI <eunji6986@naver.com>
2022-05-06 02:46:17 +0900

    #22-05-06 #김영한스프링입문 #회원관리예제시작

--- src/main/java/lovepower/hellospring/controller/HelloController.java
+++ src/main/java/lovepower/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package lovepower.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,4 +15,39 @@ public class HelloController {
 
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name="name",required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello"+name;
+    }
+
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hi = new Hello();
+        hi.setName(name);
+        hi.name="엉덩이폭탄대작전 ";
+        return hi; //객체를 넘겼다 json방식으로 넘어감
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
---
+++ src/main/java/lovepower/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package lovepower.hellospring.repository;
+
+
+import lovepower.hellospring.domain.Member;
+
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+
+}
